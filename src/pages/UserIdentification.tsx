949b88f3d982ec9b3d3c6462d574c2e53d6f7708
xnauax <62560321+xnauax@users.noreply.github.com>
2021-05-09 20:19:22 -0300

    Aula 05

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,144 @@
+import { useNavigation } from '@react-navigation/core';
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Alert
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Button } from '../components/Button'
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function UserIdentification() {
+    const navigation = useNavigation();
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [name, setName] = useState<string>()
+    function handleInputBlur() {
+        setIsFocused(false);
+        setIsFilled(!!name);
+    }
+    function handleInputFocus() {
+        setIsFocused(true);
+    }
+    function handleInputChange(value: string) {
+        setIsFilled(!!value);
+        setName(value);
+    }
+    function handleSubmit() {
+        if(!name)
+         return Alert.alert("Me diz como chamar você😝");
+        try {
+            AsyncStorage.setItem('@plantmanager:user', name)
+            navigation.navigate('Confirmation', 
+            {
+                title : 'Prontinho', 
+                subTitle: 'Agora vamos começar a cuidas das suas plantinhas com muito cuidado.',
+                buttonTitle: 'Começar',
+                icon: 'smile',
+                nextScreen: 'PlantSelect'
+            })
+        } catch {
+            Alert.alert("Não foi possível salvar o seu nome 😓");
+             
+        }
+        
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    {isFilled ? '😀' : '🤔'}
+                                </Text>
+                                <Text style={styles.title}>
+                                    Como podemos{'\n'}
+                            chamar você?
+                        </Text>
+                            </View>
+                            <TextInput
+                                placeholder="Digite um nome"
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled) && { borderColor: colors.green }
+                                ]}
+
+                                onBlur={handleInputBlur}
+                                onFocus={handleInputFocus}
+                                onChangeText={handleInputChange}
+                            />
+                            <View style={styles.footer}>
+                                <Button title="Confirmar" onPress={handleSubmit} />
+                            </View>
+
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    content: {
+        flex: 1,
+        width: '100%'
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 54,
+        alignItems: 'center',
+    },
+    header: {
+        alignItems: 'center'
+    },
+    emoji: {
+        fontSize: 44
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.gray,
+        color: colors.heading,
+        width: '100%',
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center',
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        textAlign: 'center',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        marginTop: 20
+    },
+    footer: {
+        marginTop: 40,
+        width: '100%',
+        paddingHorizontal: 20
+
+    }
+
+})
